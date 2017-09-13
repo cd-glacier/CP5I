3de72914e6fe29fcecf82bea01701416eb189594
g-hyoga <hyoga0216@gmail.com>
2017-09-13 23:24:29 +0900

    sample_json

---
+++ server/public/sample_json.js
@@ -0,0 +1,27 @@
+const recipe = {
+  id: 1,
+  name: "たいのホイル焼き",
+  pruducer_id: 5678,
+  difficuly: 20,
+  time: "01:30:00",
+  ingredients: [
+    {
+      name: "たい",
+      quantity: "2尾",
+    },
+    {
+      name: "バター",
+      quantity: "20g",
+    },
+  ],
+  method: [
+    {
+      image_id: 1234,
+      content: "アルミホイルにバターを塗ります",
+    },
+    {
+      image_id: 2345,
+      "content": "えのき、たいの順でアルミホイルにのせます",
+    },
+  ],
+}
