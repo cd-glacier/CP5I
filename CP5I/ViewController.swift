2a7e8c70de49576fffd677b18284a77f0fa3652e
g-hyoga <hyoga0216@gmail.com>
2017-09-14 15:04:48 +0900

    get /api/easy/recipe

--- CP5I/ViewController.swift
+++ CP5I/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
@@ -19,7 +20,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         Alamofire.request("http://noticeweb.net/api/easy/recipe").responseJSON { response in
-            print(response)
+            let json = JSON(response.result.value)
+            json["data"].forEach{(_, data) in
+                let type = data["name"].string!
+                self.textView.text = String(type)
+            }
         }
     }
 
