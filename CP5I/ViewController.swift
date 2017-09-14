b548065a33d5e082d59d06189deb2ecdec2be6d4
g-hyoga <hyoga0216@gmail.com>
2017-09-14 17:33:57 +0900

    ...

--- CP5I/ViewController.swift
+++ CP5I/ViewController.swift
@@ -10,28 +10,54 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
 
-    
-    @IBOutlet weak var textView: UITextView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        Alamofire.request("http://noticeweb.net/api/easy/recipe").responseJSON { response in
-            let json = JSON(response.result.value)
-            json["data"].forEach{(_, data) in
-                let type = data["name"].string!
-                self.textView.text = String(type)
-            }
-        }
-    }
+	@IBOutlet weak var searchBar: UISearchBar!
+	@IBOutlet weak var label: UILabel!
+	@IBOutlet weak var tableView: UITableView!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+	var tableData: [String] = []
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+
+		searchBar.placeholder = "検索"
+		searchBar.delegate = self
+
+		tableView.delegate = self
+		tableView.dataSource = self
+
+		Alamofire.request("http://noticeweb.net/api/easy/recipe").responseJSON { response in
+			let json = JSON(response.result.value)
+
+			json["data"].forEach{(_, data) in
+				self.tableData.append(data["name"].string!)
+                self.tableView.reloadData()
+			}
+		}
+	}
+
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+		label.text = searchBar.text
+		//キーボードを閉じる
+		self.view.endEditing(true)
+	}
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return tableData.count
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "myCell")
+		cell.textLabel?.text = tableData[indexPath.row]
+		return cell
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
 
 
 }
---
+++ Makefile
@@ -0,0 +1,4 @@
+
+setup:
+	sudo gem install cocoapods
+	pod install
